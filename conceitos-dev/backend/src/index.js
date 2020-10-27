e0072fad5eb2725ebe57836751e5a3a44b6746d1
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2020-10-27 00:08:04 -0300

    nodemon

--- conceitos-dev/backend/src/index.js
+++ conceitos-dev/backend/src/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.get('/projects', (request, response) => {
-    return response.send('Ola mundo');
+app.get('/', (request, response) => {
+    return response.json({
+        message: 'Hello world'
+    });
 })
 
 app.listen(3333);
